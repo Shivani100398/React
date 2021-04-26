2250be8438bf3fa3fb3b7ba26fa6d4bb06d28d1a
unknown <ibmjavanh18@iiht.tech>
2021-04-26 17:06:24 +0530

    added create form with ajax call

--- src/userform/userform.js
+++ src/userform/userform.js
@@ -1,21 +1,37 @@
 import { useState } from "react";
+import axios from "axios";
 
 function Userform(){
   const[userform, setUserform]=useState({
-        name:'Ram'
-  })
+        name:'Ram',
+        phone:'1'
+  });
+  const save=function(event){
+      console.log(userform.name);
+      axios.post('http://localhost:8080/user',userform)
+      .then(function(response)
+      {console.log(response);
+    })
+      .catch(function(error){
+        console.log(error);
+      });
+     
+  }
  return(
   <div>
-   <input value={userform.name} onChange={handleEvent}></input>
+   <input value={userform.name} name='name' placeholder='name'onChange={updateState}></input>
+   <input value={userform.phone} name='phone' placeholder='phone Number' onChange={updateState}></input>
+   <button onClick={save}>SAVE</button>
   </div>
   
  )
- function handleEvent(event){
-     console.log(event)
-     setUserform({
-         name:event.target.value
-     })
 
+ function updateState(event){
+     const current={...userform,
+         [event.target.name]:event.target.value};
+         console.log(current);
+         setUserform(current);
  }
+ 
  }
 export default Userform;
